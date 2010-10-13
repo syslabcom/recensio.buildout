90fadca07faa7bb67934028a74791acd4f7b5faf
gerken <gerken@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-10-13 12:59:34 +0000

    Adding production to fabfile
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.buildout/trunk@6699 51ee9ba4-e84d-4896-84de-354f1821c169

--- fabfile.py
+++ fabfile.py
@@ -10,8 +10,9 @@ def reloadProfilesAndResetCatalog():
     """ Only update profiles """
     webpass = config.get('instance-settings', 'user').split(':')[1]
     with cd(env.path):
-        run(env.serverurl % \
-            (env.webuser, webpass, ' '.join(env.profiles)))
+        sudo(env.serverurl % \
+            (env.webuser, webpass, ' '.join(env.profiles)),
+            user = env.sudouser)
 
 def createSite():
     """ Create a new recensio site. Does not delete old one! """
@@ -19,23 +20,26 @@ def createSite():
     _build()
     webpass = config.get('instance-settings', 'user').split(':')[1]
     with cd(env.path):
-        run(env.create_page_command % \
-            (env.webuser, webpass))
+        sudo(env.create_page_command % \
+            (env.webuser, webpass),
+            user = env.sudouser)
     reloadProfilesAndResetCatalog()
 
 def deleteEverythingAndRebuild():
     """ Deletes you checkout and rebuilds it. DANGEROUS"""
     with cd(env.path):
-        run('./bin/supervisorctl shutdown || echo Ignoring Error')
-        run('rm -rf parts var')
+        sudo('./bin/supervisorctl shutdown || echo Ignoring Error',
+             user = env.sudouser)
+        sudo('rm -rf parts var', user = env.sudouser)
     createSite()
 
 def _build():
     with cd(env.path):
-        run('./bin/supervisorctl shutdown || echo Ignoring Error')
-        run('./bin/buildout -c ' + env.buildoutcfg)
-        run('./bin/supervisord || echo Ignoring Error')
-    run('sleep 20')
+        sudo('./bin/supervisorctl shutdown || echo Ignoring Error',
+             user = env.sudouser)
+        sudo('./bin/buildout -c ' + env.buildoutcfg, user = env.sudouser)
+        sudo('./bin/supervisord || echo Ignoring Error' user = env.sudouser)
+    sudo('sleep 20', user = env.sudouser)
 
 def full_update():
     """ A full update """
@@ -46,8 +50,8 @@ def full_update():
 def update():
     """ Update svn and development eggs """
     with cd(env.path):
-        run('svn up')
-        run('./bin/develop update -f')
+        sudo('svn up', user = env.sudouser)
+        sudo('./bin/develop update -f', user = env.sudouser)
 
 def withDemoContent():
     """ The next steps will also add example data """
@@ -59,6 +63,7 @@ def local():
     """ Work on local environment """
     env.hosts = [config.get('local', 'host')]
     env.webuser = 'admin'
+    env.sudouser = config.get('local', 'sudouser')
     env.path = config.get('local', 'path')
     env.serverurl = './bin/recensio-policy-reset http://127.0.0.1:8010/recensio %s %s %s'
     env.create_page_command = './bin/createSite http://127.0.0.1:8010 %s %s'
@@ -68,15 +73,27 @@ def test():
     """ Work on test environment """
     env.hosts = ['zope@ext4.syslab.com']
     env.webuser = 'admin'
+    env.sudouser = 'zope'
     env.path = '/home/zope/recensio'
     env.serverurl = './bin/recensio-policy-reset http://recensio.syslab.com %s %s %s'
     env.create_page_command = './bin/createSite http://recensio.syslab.com:8012 %s %s'
     env.buildoutcfg = 'test-env.cfg'
 
+def production():
+    """ Work on production environment """
+    env.hosts = ['%s@recensio00.syslab.com' % config.get('production', 'user')]
+    env.webuser = 'admin'
+    env.sudouser = 'recensio'
+    env.path = '/home/recensio/recensio'
+    env.serverurl = './bin/recensio-policy-reset http://recensio00.syslab.com:8080/recensio %s %s'
+    env.create_page_command = './bin/createSite http://recensio.syslab.com:8080 %s %s'
+    env.buildoutcfg = 'production-env.cfg'
+
 def demo():
     """ Work on test environment """
     env.hosts = ['zope@ext4.syslab.com']
     env.webuser = 'admin'
+    env.sudouser = 'zope'
     env.path = '/home/zope/recensio_demo'
     env.serverurl = './bin/recensio-policy-reset http://recensio.syslab.com:8013/recensio %s %s %s'
     env.create_page_command = './bin/createSite http://recensio.syslab.com:8013 %s %s recensio.policy:demo'
