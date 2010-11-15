01cc10f17693796b11ea9486164c131d7e805305
gerken <gerken@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-11-15 17:27:54 +0000

    better structure
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.buildout/trunk@6947 51ee9ba4-e84d-4896-84de-354f1821c169

--- translation_complete.sh
+++ translation_complete.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-mkdir lala; ./bin/translation-extract -o lala;test `diff lala/recensio.pot src/recensio.translations/recensio/translations/locales/recensio.pot  | wc | sed -e 's/ *\([0-9]*\).*/\1/'` -lt 5
+mkdir lala; 
+./bin/translation-extract -o lala;
+BLA=`diff lala/recensio.pot src/recensio.translations/recensio/translations/locales/recensio.pot  | wc | sed -e 's/ *\([0-9]*\).*/\1/'`
+echo $BLA "<= 4"
+test $BLA -lt 5
