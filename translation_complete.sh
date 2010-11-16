0385ad047e968587a7de81971faa754c9c94d8d8
gerken <gerken@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-11-16 10:54:41 +0000

    Better dir, better output
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.buildout/trunk@6957 51ee9ba4-e84d-4896-84de-354f1821c169

--- translation_complete.sh
+++ translation_complete.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-mkdir lala; 
-./bin/translation-extract -o lala;
-BLA=`diff lala/recensio.pot src/recensio.translations/recensio/translations/locales/recensio.pot  | wc | sed -e 's/ *\([0-9]*\).*/\1/'`
-echo $BLA "<= 4"
-test $BLA -lt 5
+mkdir tmp_translations;
+./bin/translation-extract -o tmp_translations;
+grep msgid tmp_translations/recensio.pot > tmp_translations/new.pot
+grep msgid src/recensio.translations/recensio/translations/locales/recensio.pot > tmp_translations/current.pot
+BLA=`diff tmp_translations/new.pot tmp_translations/current.pot | wc | sed -e 's/ *\([0-9]*\).*/\1/'`
+diff tmp_translations/new.pot tmp_translations/current.pot
+test $BLA -lt 1
