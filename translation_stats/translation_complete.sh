39da19b81283978965ca7d089e81a90c1ee5cebd
ale-rt <alessandro.pisa@gmail.com>
2023-01-18 12:12:56 +0100

    Start fresh for Plone6

--- translation_stats/translation_complete.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -p tmp_translations;
-../bin/translation-extract -o tmp_translations;
-grep msgid tmp_translations/recensio.pot | sort > tmp_translations/new.pot
-grep msgid ../src/recensio.translations/recensio/translations/locales/templates/LC_MESSAGES/recensio.pot | sort > tmp_translations/current.pot
-BLA=`diff tmp_translations/new.pot tmp_translations/current.pot | wc | sed -e 's/ *\([0-9]*\).*/\1/'`
-diff tmp_translations/new.pot tmp_translations/current.pot
-test $BLA -lt 1
