8f6be3075eb63c3cf4282e55edd2b4515368af66
Manuel Reinhardt <reinhardt@syslab.com>
2024-03-26 07:02:49 +0100

    Add script generate_pdfs
    syslabcom/scrum#1185

---
+++ scripts/generate_pdfs.py
@@ -0,0 +1,25 @@
+import logging
+import sys
+
+from recensio.plone.subscribers.review import update_generated_pdf
+from slc.zopescript.script import InstanceScript
+from transaction import commit
+
+logger = logging.getLogger(__name__)
+
+
+class GeneratePdfs(InstanceScript):
+    def run(self):
+        reviews = self.portal.portal_catalog(
+            object_provides="recensio.plone.interfaces.IReview"
+        )
+        for brain in reviews:
+            review = brain.getObject()
+            if not getattr(review, "generatedPdf", None):
+                logger.info("Updating PDF for %s", brain.getPath())
+                update_generated_pdf(review)
+                commit()
+
+
+site = sys.argv[3]  # noqa:F821
+GeneratePdfs(app)("admin", portal_id=site)
